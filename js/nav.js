010e372a158a9259e08fcfd4960a764dccb92109
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2021-11-08 23:13:08 -0500

    added responsive nav responsive tile adaptations improved other aspects of website responsiveness

---
+++ js/nav.js
@@ -0,0 +1,24 @@
+/*code inspired from https://www.youtube.com/watch?v=gXkqy0b4M5g*/
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li')
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index)=>{
+      if(link.style.animation){
+        link.style.animation = ''
+      }
+      else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`
+      }
+    })
+
+    burger.classList.toggle('toggle')
+
+  });
+};
+
+navSlide();
